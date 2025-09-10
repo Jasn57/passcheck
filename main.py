bb57660f81b392765fb38f4d9a1ed94aa46d2d18
Jason <jasn74939@gmail.com>
2025-09-10 10:32:20 -0400

    Make work

--- main.py
+++ main.py
@@ -1,32 +1,28 @@
-# get password
-password = input("enter your password ")
+# Get password
+password = input("Enter your password: ")
 print(f'Your password is {password}')
 
-# define characters
-u_letters = ['A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z']
-l_letter = ['a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z']
-numbers =['1, 2, 3, 4, 5, 6, 7, 8, 9']
-symbols = ['!, @, #, $, %']
+# define charecters
+u_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+l_letters = "abcdefghijklmnopqrstuvwxyz"
+numbers = "1234567890"
+symbols = "!@#$%"
 
-# set default points
+# Set default points
 points = 0
 
-# add points 
-if l_letter {
-    points + 1 
-}
+# Add points based
+if any(char in l_letters for char in password):
+    points += 1
 
-if u_letters {
-    points + 2 
-}
+if any(char in u_letters for char in password):
+    points += 2
 
-if numbers {
-    points + 3
-}
+if any(char in numbers for char in password):
+    points += 3
 
-if symbols {
-    points + 4
-}
+if any(char in symbols for char in password):
+    points += 4
 
-# print total
-print(f'you have {points} points')
+# Print total
+print(f'You have {points} points')
